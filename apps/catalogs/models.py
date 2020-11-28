769a999e099936512088da335017997ca4b24e9d
Alex karakhanidi <coder.django@gmail.com>
2020-11-28 08:12:46 +0600

    added category model, migrations, admin

--- apps/catalogs/models.py
+++ apps/catalogs/models.py
@@ -1,3 +1,17 @@
+from django.utils.translation import ugettext_lazy as _
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_('Title'), max_length=150, db_index=True)
+    slug = models.SlugField(_('Slug'), max_length=150, unique=True, db_index=True)
+    order = models.PositiveIntegerField(_('Order'), default=1)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+    def __str__(self):
+        return self.name
