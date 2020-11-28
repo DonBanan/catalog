de7ee2d4ed54e29b9d70a21bf9159242d5ff50f7
Alex karakhanidi <coder.django@gmail.com>
2020-11-28 08:51:33 +0600

    added base templates

--- apps/urls.py
+++ apps/urls.py
@@ -18,9 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', IndexView.as_view(), name="index"),
+
     path('summernote/', include('django_summernote.urls')),
 ]
 
